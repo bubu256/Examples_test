b4ddaf3fc4c0b0ae2ab9067ff77f07c74e927513
rettopekim <bulostm@gmail.com>
2022-09-23 17:09:54 +0300

    add HW 1.2

---
+++ HW_1.2/Program_1.2.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine("Введите первое число: ");
+int a = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите второе число: ");
+int b = int.Parse(Console.ReadLine());
+
+if (a < b)
+{
+    Console.WriteLine($"Число {b} больше {a}");
+}
+else if (a > b)
+{
+    Console.WriteLine($"Число {a} больше {b}");
+}
+else
+{
+    Console.WriteLine("Числа равны!");
+}

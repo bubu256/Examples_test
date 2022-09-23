1a2cda05bb770fa331db3b39c446e8cdaac2250b
rettopekim <bulostm@gmail.com>
2022-09-23 12:02:45 +0300

    add Example002 and gitignore

---
+++ example002_HelloUser/Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Console.WriteLine("Hello, World!");
+Console.Write("Введите ваше имя: ");
+string name = Console.ReadLine();
+Console.Write("Привет, ");
+Console.WriteLine(name);

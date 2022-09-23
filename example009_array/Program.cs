6e59e34b8813a02eb3a3b14db5be4ea182797c80
rettopekim <bulostm@gmail.com>
2022-09-23 18:19:32 +0300

    add array and gitignore

---
+++ example009_array/Program.cs
@@ -0,0 +1,41 @@
+﻿void FillArray(int[] coll)
+{
+    int n = coll.Length;
+    int i = 0;
+    while (i<n)
+    {
+        coll[i] = new Random().Next(1, 10);
+        i++;
+    }
+}
+
+void PrintArray(int[] arr)
+{
+    int i = 0;
+    while (i < arr.Length)
+    {
+        Console.Write(arr[i]);
+        i++;
+    }
+}
+
+int IndexFind(int[] arr, int find)
+{
+    int i = 0;
+    while (i < arr.Length)
+    {
+        if (arr[i] == find) 
+        {
+            return i;
+        }
+        i++;
+    }
+    return -1;
+}
+
+int[] array = new int[10];
+
+FillArray(array);
+PrintArray(array);
+Console.WriteLine();
+Console.WriteLine(IndexFind(array, 3));

144fa54e59e62ea935ec93c486285950af38108d
rettopekim <bulostm@gmail.com>
2022-09-23 12:58:41 +0300

    add example005 condition ifelse

---
+++ example005_ifelse/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите имя пользователя: ");
+string username = Console.ReadLine();
+
+if (username.ToLower() == "маша")
+{
+    Console.WriteLine("Hello, Maria!!!");
+}
+else
+{
+    Console.Write("Hello, ");
+    Console.Write(username);
+}

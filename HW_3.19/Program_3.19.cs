4a893d3938d36105502672764097fb5cc8666a53
rettopekim <bulostm@gmail.com>
2022-09-27 14:42:51 +0300

    add HW 3.*

---
+++ HW_3.19/Program_3.19.cs
@@ -0,0 +1,23 @@
+﻿// Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+// 14212 -> нет
+// 12821 -> да
+// 23432 -> да
+
+bool IsPolyndrom(string str)
+{
+    string strRevers = String.Empty;
+    for(int i = str.Length - 1; i >= 0; i--)
+    {
+        strRevers += str[i];
+    }
+
+    if (str == strRevers) return true;
+
+    return false;
+}
+
+if(IsPolyndrom(Console.ReadLine()))
+{
+    Console.WriteLine("да");
+}
+else Console.WriteLine("нет");
---
+++ HW_3.21/Program_3.21.cs
@@ -0,0 +1,34 @@
+﻿// Задача 21
+// Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
+// A (3,6,8); B(2, 1, -7), -> 15.84
+// A(7, -5, 0); B(1, -1, 9)-> 11.53
+
+// ввод координаты точки от пользователя
+int[] InputVector()
+{
+    int[] vector = new int[3];
+    for(int i = 0; i < 3; i++)
+    {
+        vector[i] = int.Parse(Console.ReadLine());
+    }
+    return vector;
+}
+
+// подсчет расстояние между х и у
+double Distance(int[] x, int[] y)
+{
+    double result = 0;
+    for(int i = 0; i < x.Length; i++)
+    {
+        result += Math.Pow(x[i] - y[i], 2);
+    }
+    return Math.Sqrt(result);
+}
+
+Console.WriteLine("Введите координаты точки А: ");
+int[] a = InputVector();
+Console.WriteLine("Введите координаты точки B: ");
+int[] b = InputVector();
+
+Console.WriteLine($"Расстояние между точками: {Distance(a, b)}");
+
